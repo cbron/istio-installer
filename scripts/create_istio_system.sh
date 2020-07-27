3b51460f69cbdefcfc14f568c4c142fcefa58dcb
Caleb Bron <caleb@rancher.com>
2020-07-27 16:10:36 -0700

    canary

--- scripts/create_istio_system.sh
+++ scripts/create_istio_system.sh
@@ -7,7 +7,8 @@ if [[ $old_versions == *"1.3"* || $old_versions == *"1.4"* || $old_versions == *
 fi
 
 versions=$(kubectl get --ignore-not-found=true deploy istiod -n istio-system -o=jsonpath='{$.spec.template.spec.containers[*].image}')
-if [[ $versions == "" ]]
+# If canary you install. If version not found you install. Otherwise you upgrade.
+if [[ $CANARY_REVISION ]] || [[ $versions == "" ]]
 then
     if test -f "/app/overlay-config.yaml"
     then
